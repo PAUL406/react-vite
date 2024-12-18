199f14dacc34a35d0449ad5642a49464b130cf38
PAUL406 <pauul406@gmail.com>
2024-12-18 11:25:12 -0400

    03-mouse-follower

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,26 +1,56 @@
 import { useEffect, useState } from "react";
 
-function App() {
+const FollowMouse = () => {
 	const [enabled, setEnablad] = useState(false);
+	const [position, setPosition] = useState({ x: 0, y: 0 });
 
+	//pointer move
 	useEffect(() => {
-		console.log("efecto");
-	});
+		// console.log("efecto");
+		console.log("effect", { enabled });
+
+		const handleMove = (event) => {
+			const { clientX, clientY } = event;
+			// console.log("handleMove", { clientX, clientY });
+			setPosition({ x: clientX, y: clientY });
+		};
+		if (enabled) {
+			window.addEventListener("pointermove", handleMove);
+		}
+		//cleanup
+		// --> Cuando el componente se desmonta
+		// --> Cuando cambian las dependencias antes de ejecutar el effect de nuevo
+		return () => {
+			//cleanup method
+			console.log("cleanup");
+			window.removeEventListener("pointermove", handleMove);
+		};
+	}, [enabled]);
+
+	//change body classname
+	useEffect(() => {
+		document.body.classList.toggle("no-cursor", enabled);
+
+		return () => {
+			document.body.classList.remove;
+		};
+	}, [enabled]);
 
 	return (
-		<main>
+		<>
 			<div
 				style={{
 					position: "absolute",
-					backgroundColor: "#09f",
+					backgroundColor: "rgba(0, 0, 0, 0.5)",
+					border: "1px solid #fff",
 					borderRadius: "50%",
 					opacity: 0.8,
 					pointerEvents: "none",
-					left: -20,
-					top: -20,
-					width: 40,
-					height: 40,
-					transform: "translate(0px,0px)",
+					left: -25,
+					top: -25,
+					width: 50,
+					height: 50,
+					transform: `translate(${position.x}px,${position.y}px)`,
 				}}
 			></div>
 			<button
@@ -30,6 +60,17 @@ function App() {
 			>
 				{enabled ? "Desativar" : "activar"} seguir puntero
 			</button>
+		</>
+	);
+};
+
+function App() {
+	const [mounted, setMounted] = useState(true);
+
+	return (
+		<main>
+			{mounted && <FollowMouse />}
+			<button onClick={() => setMounted(!mounted)}>Toggle mounted followMous component</button>
 		</main>
 	);
 }
